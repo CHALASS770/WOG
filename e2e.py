8ca29e99ae93e9c6a7fb7691f6623bac377f7f78
CHALASS770 <54074737+CHALASS770@users.noreply.github.com>
2019-11-17 14:41:10 +0200

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -10,7 +10,7 @@ def test_score_webservice():
 
     Url = "http://127.0.0.1:8777"
     print(3)
-    driver = webdriver.Chrome(executable_path='../WOG/chromedriver')
+    driver = webdriver.Chrome(executable_path='./WOG/chromedriver')
     print(driver) 
     print(2)
     driver.get(Url)
