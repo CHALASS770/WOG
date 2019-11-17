7fc8e4b261c01c3114a83ebed2158a3945747e7c
CHALASS770 <54074737+CHALASS770@users.noreply.github.com>
2019-11-17 19:29:55 +0200

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -10,7 +10,7 @@ def test_score_webservice():
 
     Url = "http://127.0.0.1:8777"
     print(3)
-    driver = webdriver.Firefox(executable_path='../WOG/geckodriver')
+    driver = webdriver.Firefox(executable_path='../geckodriver')
     print(driver) 
     print(2)
     driver.get(Url)
@@ -29,7 +29,6 @@ def main_test() :
     try :
         test = test_score_webservice()
         print(0)
-    except:
-        print(-1)
+
 
 main_test()
